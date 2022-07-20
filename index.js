26467d654e4c6e3cc9678bd07087ca415e59d227
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2022-07-20 19:56:05 +0530

    javascript code for the selected file input

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ label.addEventListener('drop',onLeave);
 label.addEventListener('dragend',onLeave);
 label.addEventListener('dragLeave',onLeave);
 label.addEventListener('dragexit',onLeave);
-
+//to know when a file was succesfully selected
+const input = document.querySelector('input');
+input.addEventListener('change' , event=>{
+    if(input.files.length>0){
+    }
+})

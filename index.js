e271711aa3ec9ea798cbe590e906c6eb83f6fb09
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2022-07-20 19:54:00 +0530

    javascript code

---
+++ index.js
@@ -0,0 +1,15 @@
+const label = document.querySelector('label');
+
+function onEnter() {
+    label.classList.add('active');
+}
+function onLeave(){
+    label.classList.remove('active');
+}
+label.addEventListener('dragenter' , onEnter);
+
+label.addEventListener('drop',onLeave);
+label.addEventListener('dragend',onLeave);
+label.addEventListener('dragLeave',onLeave);
+label.addEventListener('dragexit',onLeave);
+
